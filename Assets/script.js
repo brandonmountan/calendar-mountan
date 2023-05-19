e9b1023efc194f1373eca23e34dc2fc8010ee025
Brandon Mountan <brandonmountan1@gmail.com>
2023-05-18 19:35:48 -0600

    final commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,74 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-// Puts current date and time into website
+// Displays current date and time into website
 var today = dayjs();
 var currentHour = dayjs().hour();
 var time = dayjs().format('hh:mm:ss');
 $('#currentDay').text(today.format('MMM D, YYYY, hh:mm:ss'));
 
 
+    //A function being called for every row class div (each time block)
+$(".row").each(function() {
+    //Using parse to make the id a number
+  var hour = parseInt($(this).attr("id"));
+    //Retrieving previously saved item from local storage
+  var goodbye = localStorage.getItem(hour);
+    //Loop to add/remove classes when comparing actual time to hour in calendar
+  if (currentHour < hour) {
+  $(this).removeClass("past present").addClass("future");
+  } else if (currentHour > hour) {
+  $(this).removeClass("future present").addClass("past");  
+  } else {
+  $(this).removeClass("past future").addClass("present");
+  }
 
-  $(".row").each(function() {
-    //Breaking th
-    var hour = parseInt($(this).attr("id"));
-
-    var goodbye = localStorage.getItem(hour);
-
-    if (currentHour < hour) {
-      
-    $(this).removeClass("past present").addClass("future");
-    } else if (currentHour > hour) {
-      
-    $(this).removeClass("future present").addClass("past");  
-    } else {
-      
-    $(this).removeClass("past future").addClass("present");
-    }
-
-
-    $(this).children(".description").text(goodbye);
-
-    $(this).children(".saveBtn").click(function (event) {
-
-      event.stopPropagation();
-  
-      var goodbye = $(this).siblings(".description").val();
-  
-        console.log(goodbye);
-  
-      localStorage.setItem(hour, goodbye);
-  
-    });
-    })
-
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-// $(function addLabel() {
-
-//   if (time < hour) {
-//     $("div").addClass("past");
-//   } if (time === hour) {
-//     $("div").addClass("present");
-//   } if (time > hour) {
-//     $("div").addClass("future");
-//   }
-// });
-
-  // if time > hour then past
-  // if time = hour then present
-  // if time < hour then future
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// }));
+  $(this).children(".description").text(goodbye);
+    //A function to save the user inputed text
+  $(this).children(".saveBtn").click(function (event) {
+    event.stopPropagation();
+    var goodbye = $(this).siblings(".description").val();
+    localStorage.setItem(hour, goodbye);
+  });
+})
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # calendar-mountan
+
+## Description
+
+The motivation behind building this project was to use what we leanred in week 5 (Third Party API's) and apply it to a real project. The main third party api that was used was jQuery. jQuery has a different syntax so there was a little bit of a struggle to get everything to function correctly. I built this project so I could produce a website that provides 24 1 hour time blocks for the current day that users could save things to the specific hour. I learned a lot about jQuery as well as how to use day.js.
+
+## Screenshot
+
+![screenshot page 1](/Assets/images/project5img1.PNG);
+
+![screenshot page 1](/Assets/images/project5img2.PNG);
+
+
+## Link
+
+https://brandonmountan.github.io/calendar-mountan/
