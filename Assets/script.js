e97de273255335ce97d29c14ab5217f7e1b797f3
Brandon Mountan <brandonmountan1@gmail.com>
2023-05-18 18:56:05 -0600

    post tutor session

--- Assets/script.js
+++ Assets/script.js
@@ -4,92 +4,46 @@
 
 // Puts current date and time into website
 var today = dayjs();
+var currentHour = dayjs().hour();
 var time = dayjs().format('hh:mm:ss');
 $('#currentDay').text(today.format('MMM D, YYYY, hh:mm:ss'));
 
 
 
+  $(".row").each(function() {
+    //Breaking th
+    var hour = parseInt($(this).attr("id"));
 
-var userInput = document.querySelectorAll(".description");
-var saveButton = document.querySelectorAll(".saveBtn");
+    var goodbye = localStorage.getItem(hour);
 
-
-
-
-// console.log(userInput);
-// console.log(saveButton);
-
-// renderLastSaved();
-
-// function renderLastSaved () {
-
-//   var goodbye = localStorage.getItem("hello");
-
-//   for (let i = 0; i < userInput.length; i++) {
-
-//     userInput[i].textContent = goodbye;
-
-//   };
-//   console.log(goodbye);
-// };
-
-$(function testing() {
-
-    saveButton.onclick(function(event) {
-      event.preventDefault();   
-      console.log(event.target);
-      // var hello = $(userInput).val();
-      // console.log(hello);
-      // localStorage.setItem("hello", hello);
-
-    // function renderLastSaved() {
-    //   for (let i = 0; i < userInput.length; i++) {
-
-    //       userInput[i].textContent = goodbye;
-
-    //       localStorage.setItem("goodbye", goodbye);
-
-    //       console.log(goodbye);
-
-    //   }
-    //   // console.log(goodbye);
-    //   // for (let i = 0; i < saveButton.length; i++) {
-
-    //   //   saveButton[i].event = hey;
-
-    //   // }
-
-    // }
-    //   var goodbye = localStorage.getItem("goodbye");
-
-    //   renderLastSaved();
-    // })
-});
-})
-
-// $(saveButton.addEventListener("click", function(event) {
-//     // event.preventDefault();
-//     console.log(event);
-
-//     var description = localStorage.getItem("description");
-//     userInput.textContent = description;
-//     console.log(description);
-
-//     if (description === "") {
-//       alert("test");
-//       localStorage.setItem("description", description);
-//       renderLastSaved();
-//     } else {
-//       alert("hello");
-//     }
+    if (currentHour < hour) {
+      
+    $(this).removeClass("past present").addClass("future");
+    } else if (currentHour > hour) {
+      
+    $(this).removeClass("future present").addClass("past");  
+    } else {
+      
+    $(this).removeClass("past future").addClass("present");
+    }
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+    $(this).children(".description").text(goodbye);
+
+    $(this).children(".saveBtn").click(function (event) {
+
+      event.stopPropagation();
+  
+      var goodbye = $(this).siblings(".description").val();
+  
+        console.log(goodbye);
+  
+      localStorage.setItem(hour, goodbye);
+  
+    });
+    })
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
